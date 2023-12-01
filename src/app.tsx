3bf047d89595f6c05e526674afd4d83f4c71195d
Boris Serdiuk <just-boris@hotmail.com>
2023-12-01 11:02:41 +0000

    Update 2 files

--- src/app.tsx
+++ src/app.tsx
@@ -1,53 +1,23 @@
-import AppLayout from "@cloudscape-design/components/app-layout";
-import Box from "@cloudscape-design/components/box";
-import Button from "@cloudscape-design/components/button";
-import Container from "@cloudscape-design/components/container";
-import Header from "@cloudscape-design/components/header";
-import SideNavigation from "@cloudscape-design/components/side-navigation";
-import styles from "./app.module.scss";
+import Select from "@cloudscape-design/components/select";
 
-export function App() {
+interface EnumSelectProps<OptionType> {
+  selectedOption: OptionType | null;
+  options: Array<OptionType>;
+  onChange: (newOption: OptionType) => void;
+  getLabel: (option: OptionType) => string;
+}
+
+function EnumSelect<OptionType extends string>({
+  selectedOption,
+  options,
+  onChange,
+  getLabel,
+}: EnumSelectProps<OptionType>) {
   return (
-    <AppLayout
-      navigation={
-        <SideNavigation
-          header={{ text: "Service", href: "" }}
-          items={[{ type: "link", text: "Home", href: "" }]}
-          activeHref=""
-        />
-      }
-      toolsHide={true}
-      contentHeader={
-        <Header
-          variant="h1"
-          description={
-            <>
-              Using <Box variant="code">@cloudscape-design/components</Box> library
-            </>
-          }
-        >
-          Demo app
-        </Header>
-      }
-      content={
-        <Container
-          header={
-            <Header
-              actions={
-                <Button data-testid="action-button" variant="primary" onClick={() => alert("Not implemented")}>
-                  Action
-                </Button>
-              }
-            >
-              Main content
-            </Header>
-          }
-        >
-          <div className={styles.contentWrapper}>
-            <div>Your application content goes here...</div>
-          </div>
-        </Container>
-      }
+    <Select
+      selectedOption={selectedOption && { value: selectedOption }}
+      options={options.map((value) => ({ value, label: getLabel(value) }))}
+      onChange={(event) => onChange(event.detail.selectedOption.value as OptionType)}
     />
   );
 }
